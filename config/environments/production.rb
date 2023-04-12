b58b17525f9bc45560d19a489cb8cf527120a11a
Kevin Zheng <kevzheng27@gmail.com>
2023-04-11 20:16:11 -0400

    Uncomment default routes in production rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,6 +112,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  # config.action_mailer.default_url_options = { host: "https://demo-project-ecommerce.onrender.com" }
-  # Rails.application.routes.default_url_options[:host] = "https://demo-project-ecommerce.onrender.com"
+  config.action_mailer.default_url_options = { host: "https://demo-project-ecommerce.onrender.com" }
+  Rails.application.routes.default_url_options[:host] = "https://demo-project-ecommerce.onrender.com"
 end
